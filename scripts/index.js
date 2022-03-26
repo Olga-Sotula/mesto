0c475c6cf04d62b170805b460579ac82e49aad70
Olga Sotula <olga.sotula@itctg.ru>
2022-03-26 08:43:41 +0300

    feat: add handlers for popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,34 @@
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 const popupWindow = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
 const profileEditButton = document.querySelector('.profile__edit-button');
+const popupForm = document.querySelector('.popup__form');
+const popupInputName = popupForm.querySelector('.popup__input_type_name');
+const popupInputDescription = popupForm.querySelector('.popup__input_type_description');
 
 function togglePopupWindow() {
   popupWindow.classList.toggle('popup_opened');
 }
 
-popupCloseButton.addEventListener('click', togglePopupWindow);
-profileEditButton.addEventListener('click', togglePopupWindow);
-
-function onOverlayClick(e) {
-  if (e.target === e.currentTarget) {
-    togglePopupWindow();
-  }
+function onOpenPopupWindow(){
+  togglePopupWindow();
+  popupInputName.value = profileTitle.textContent;
+  popupInputDescription.value = profileSubtitle.textContent;
 }
 
-popupWindow.addEventListener('click', onOverlayClick);
+function onClosePopupWindow(){
+  togglePopupWindow();
+}
+
+function formSubmitHandler(evt){
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  profileTitle.textContent = popupInputName.value;
+  profileSubtitle.textContent = popupInputDescription.value;
+  togglePopupWindow();
+}
+
+popupCloseButton.addEventListener('click', onClosePopupWindow);
+profileEditButton.addEventListener('click', onOpenPopupWindow);
+popupForm.addEventListener('submit',formSubmitHandler);
+
