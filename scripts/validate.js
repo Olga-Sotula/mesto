9e94ee3e26aeb31c847ea84998311d0eb963abc3
Olga Sotula <olga.sotula@itctg.ru>
2022-04-20 15:44:31 +0300

    feat: validation. add novalidate to forms

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,30 +1,30 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
-  /*const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('form__input_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('form__input-error_active');*/
+const showInputError = (formElement, inputElement, errorMessage,inputErrorClass) => {
+  //const errorElement = formElement.querySelector(inputErrorClass);
+  inputElement.classList.add(inputErrorClass);
+  //errorElement.textContent = errorMessage;
+  //errorElement.classList.add('form__input-error_active');
 };
 
-const hideInputError = (formElement, inputElement) => {
-  /*const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('form__input_type_error');
-  errorElement.classList.remove('form__input-error_active');
-  errorElement.textContent = '';*/
+const hideInputError = (formElement, inputElement, inputErrorClass) => {
+  //const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  //errorElement.classList.remove('form__input-error_active');
+  //errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement,inputErrorClass) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage, inputErrorClass);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, inputErrorClass);
   }
 };
 
-const setEventListeners = (formElement, inputSelector) => {
+const setEventListeners = (formElement, inputSelector, inputErrorClass) => {
   const inputList = Array.from(formElement.querySelectorAll(inputSelector));
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
+      checkInputValidity(formElement, inputElement, inputErrorClass);
     });
   });
 };
@@ -32,10 +32,10 @@ const setEventListeners = (formElement, inputSelector) => {
 const enableValidation = (elementSelectors) => {
   const formList = Array.from(document.querySelectorAll(elementSelectors.formSelector));
   formList.forEach((formElement) => {
-    /*formElement.addEventListener('submit', (evt) => {
+    formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
-    });*/
+    });
 
-      setEventListeners(formElement, elementSelectors.inputSelector);
+      setEventListeners(formElement, elementSelectors.inputSelector, elementSelectors.inputErrorClass);
   });
 };
