12c778d57e0936b2a6a28f1f75642a0f8688e5e1
Olga Sotula <olga.sotula@itctg.ru>
2022-05-25 17:45:12 +0300

    style: add Popup class

---
+++ src/components/Popup.js
@@ -0,0 +1,34 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._closeButton = this._popup.querySelector('popup__close');
+  }
+
+  _handleOverlayClick(evt) {
+    if (evt.target === evt.currentTarget) {
+      this.close();
+    }
+  }
+
+  _handleEscClose() {
+    if (evt.key === 'Escape'){
+      this.close();
+    }
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    this._popup.addEventListener('mousedown', (evt) => {this._handleOverlayClick(evt)});
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    this._popup.removeEventListener('mousedown', this._handleOverlayClick);
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  setEventListeners() {
+    this._closeButton.addEventListener('click', (evt) => {this._handleOverlayClick(evt)});
+  }
+}
