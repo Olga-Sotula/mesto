cc8d899be544b53d7f951c8cace4e584e3697211
Olga Sotula <olga.sotula@itctg.ru>
2022-06-14 10:42:34 +0300

    style: separate _handleResponse func

--- src/components/Api.js
+++ src/components/Api.js
@@ -7,43 +7,31 @@ export default class Api {
     }
   }
 
+  _handleResponse(res, err){
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(err);
+  }
+
   getUserInfo() {
     return fetch(`${this._url}/users/me`, {
         headers: this._header
       })
       .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject('Ошибка загрузки информации о пользователе');
+        return this._handleResponse(res, 'Ошибка загрузки информации о пользователе');
       })
   }
 
   updateUserProfile(profile) {
-    return fetch(`${this._url}/users/me`, {
+    return fetch(`${this._url}/users/m`, {
         method: 'PATCH',
         headers: this._header,
         body: JSON.stringify(profile)
       })
       .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject('Ошибка обновления профайла');
+        return this._handleResponse(res,'Ошибка обновления профайла');
       })
-    fetch('https://mesto.nomoreparties.co/v1/cohortId/', {
-      method: 'PATCH',
-      headers: {
-        authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: 'Marie Skłodowska Curie',
-        about: 'Physicist and Chemist'
-      })
-    });
   }
 
 
@@ -52,11 +40,7 @@ export default class Api {
         headers: this._header
       })
       .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-
-        return Promise.reject('Ошибка загрузки карточек');
+        return this._handleResponse(res,'Ошибка загрузки карточек');
       })
   }
 }
